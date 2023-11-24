cf449fac6a96db76ca5acc897cba30f71db2d9d7
박소영 <48235429+soy6032@users.noreply.github.com>
2023-11-24 20:24:49 +0900

    [Feat] : 백그라운드 변경
    [Feat] : 백그라운드 변경

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -4,14 +4,15 @@ import style from '../../style/user/Login.module.css'
 import kakao from '../../assets/img/user/kakaoLogin.png'
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import back from '../../assets/img/back.png'
 
 function Login() {
   return (
     <div id={style.loginWrap}>
-      <h1 className={`${style.title}`+ ' textCenter textBold'}>로그인</h1>
+      <h1 className={`${style.title}`+ ' textCenter textBold'}>LOGIN</h1>
       <p></p>
       <img src={kakao} alt="카카오 로그인" className={`${style.kakaoLogin}`}/>
-      <label>이메일</label>
+      <label style={{fontSize: '10px'}}>이메일</label>
       <input type='text' className={style.input} style={{marginBottom:'20px'}}/>
       <label>패스워드</label>
       <input type='text' className={style.input}/>
@@ -19,6 +20,7 @@ function Login() {
       <Button variant="contained" href="#contained-buttons" sx={{marginTop:'40px', width:'100%', padding:'11.5px 14px', backgroundColor:'#243579'}}>
         Login
       </Button>
+      <img src={back} alt="카카오 로그인" className={style.back}/>
     </div>
   );
 }
