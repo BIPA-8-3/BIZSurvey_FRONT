65f9805dcb5422c0fbef02a312b02a3232473c90
박소영 <48235429+soy6032@users.noreply.github.com>
2023-11-28 12:29:15 +0900

    [Feat] : 커뮤니티 상세, 댓글 레이아웃
    [Feat] : 커뮤니티 상세, 댓글 레이아웃

--- src/components/community/CommunityTable.jsx
+++ src/components/community/CommunityTable.jsx
@@ -4,6 +4,7 @@ import style from"../../style/community/Table.module.css"
 import IconWithText from '../common/IconWithText';
 import logo from "../../assets/img/avatar.png"
 import { CiMemoPad } from "react-icons/ci";
+import { Link } from 'react-router-dom';
 
 function CommunityTable() {
   return (
@@ -23,7 +24,7 @@ function CommunityTable() {
                     <td style={{width:'10%'}}>1</td>
                     <td >
                         <span className={style.best}>[Best]</span>
-                        비즈서베이의 커뮤니티 게시글:)
+                        <Link to={'/communityDetail'}>비즈서베이의 커뮤니티 게시글:)</Link>
                         <span className={style.comment}>[52]</span>
                         <span className={style.new}>new</span>
                     </td>
@@ -42,7 +43,7 @@ function CommunityTable() {
                     <td style={{width:'10%'}}>2</td>
                     <td >
                         <span className={style.best}>[Best]</span>
-                        아무말이나 그냥 적어서 테스트 해보는거야 왜냐하면 글자가 없으면 이상해보이니깐ㅎ
+                        <Link to={'/communityDetail'}>아무말이나 그냥 적어서 테스트 해보는거야 왜냐하면 글자가 없으면 이상해보이니깐ㅎ</Link>
                         <span className={style.comment}>[154]</span>
                         <span className={style.vote}>
                             <IconWithText className='item1' text={'투표'} fontsize={'12px'} fontweight={'700'} fontcolor={'#fff'}>
@@ -66,7 +67,7 @@ function CommunityTable() {
                     <td style={{width:'10%'}}>3</td>
                     <td >
                         <span className={style.best}>[Best]</span>
-                        아무말이나 그냥 적어서 테스트 해보는거야 왜냐하면 글자가 없으면 이상해보이니깐ㅎ
+                        <Link to={'/communityDetail'}>아무말이나 그냥 적어서 테스트 해보는거야 왜냐하면 글자가 없으면 이상해보이니깐ㅎ</Link>
                         <span className={style.comment}>[154]</span>
                         <span className={style.new}>new</span>
                     </td>
@@ -83,7 +84,8 @@ function CommunityTable() {
                 </tr>
                 <tr>
                     <td style={{width:'10%'}}>4</td>
-                    <td >비즈서베이의 커뮤니티 게시글:)
+                    <td >
+                        <Link to={'/communityDetail'}>비즈서베이의 커뮤니티 게시글:)</Link>
                         <span className={style.comment}>[52]</span>
                         <span className={style.new}>new</span>
                     </td>
@@ -102,7 +104,7 @@ function CommunityTable() {
                     <td style={{width:'10%'}}>5</td>
                     <td>
                         <span className={style.best}>[Best]</span>
-                        아무말이나 그냥 적어서 테스트 해보는거야 왜냐하면 글자가 없으면 이상해보이니깐ㅎ
+                        <Link to={'/communityDetail'}>아무말이나 그냥 적어서 테스트 해보는거야 왜냐하면 글자가 없으면 이상해보이니깐ㅎ</Link>
                         <span className={style.comment}>[154]</span>
                     <span className={style.vote}>
                         <IconWithText className='item1' text={'투표'} fontsize={'12px'} fontweight={'700'} fontcolor={'#fff'}>
