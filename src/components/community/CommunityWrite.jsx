31ac4c95dad6543fa95be0182bb23266e2de13e4
박소영 <48235429+soy6032@users.noreply.github.com>
2023-11-28 18:35:08 +0900

    [Refactor] : Toast 에디터 제거
    [Refactor] : Toast 에디터 제거

--- src/components/community/CommunityWrite.jsx
+++ src/components/community/CommunityWrite.jsx
@@ -6,12 +6,7 @@ import useFadeIn from '../../style/useFadeIn';
 import back from '../../assets/img/back.png';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
-import { Editor } from '@toast-ui/react-editor';
-// import '@toast-ui/editor/dist/toastui-editor.css';
-import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
-// import 'tui-color-picker/dist/tui-color-picker.css';
-// import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
-// import '@toast-ui/editor/dist/i18n/ko-kr';
+
 import CreateVote from './CreateVote';
 
 export default function CommunityWrite() {
