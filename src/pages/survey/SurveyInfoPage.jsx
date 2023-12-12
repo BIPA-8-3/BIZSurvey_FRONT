69fde40dccdd067654c3e9d53ffb64f4c2592a21
tud0 <psy563012@gmail.com>
2023-12-12 14:55:57 +0900

    [Feat] : 설문 생성 모달

--- src/pages/survey/SurveyInfoPage.jsx
+++ src/pages/survey/SurveyInfoPage.jsx
@@ -82,7 +82,7 @@ export default function SurveyInfoPage() {
   return (
     <>
       <SurveyContext.Provider value={contextValue}>
-        <div>
+        <div style={{ marginTop: "20px" }}>
           <div style={{ width: "700px", margin: "0 auto" }}>
             <ButtonTab
               handleChangeTab={handleChangeTab}
