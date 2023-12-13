c6196672e347558d640d5bbc5f4fc05b422cc97c
tud0 <psy563012@gmail.com>
2023-12-13 09:54:26 +0900

    [Refactor] : 설문 리스트 모달창 수정

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,8 +1,4 @@
 import { lazy } from "react";
-import { Navigate } from "react-router-dom";
-import SurveyInfoPage from "../pages/survey/SurveyInfoPage";
-import SurveyResultPage from "../pages/survey/SurveyResultPage";
-import EditSurveyPage from "../pages/survey/EditSurveyPage";
 
 const FullLayout = lazy(() => import("../pages/Layout"));
 
@@ -39,15 +35,23 @@ const EditScoreSurvey = lazy(() =>
   import("../pages/survey/EditScoreSurveyPage")
 );
 const PostDetail = lazy(() => import("../components/community/PostDetail"));
-const SurveyCommunityWrite = lazy(() => import('../components/surveyCommunity/SurveyCommunityWrite'))
-const OAuth2RedirectHandler = lazy(() => import('../components/common/OAuth2RedirectHandler'))
-const EmailValidation = lazy(() => import('../components/user/EmailValidation'))
-const ChangPassword = lazy(() => import('../components/user/ChangePassword'))
-const AdditionalJoin = lazy(() => import('../components/user/AdditionalJoin'))
-const CommunitySearchResult = lazy(() => import('../components/common/SearchResultTable'))
-const SCommunitySearchResult = lazy(() => import('../components/common/SCommunitySearchResult')) 
-
-
+const SurveyCommunityWrite = lazy(() =>
+  import("../components/surveyCommunity/SurveyCommunityWrite")
+);
+const OAuth2RedirectHandler = lazy(() =>
+  import("../components/common/OAuth2RedirectHandler")
+);
+const EmailValidation = lazy(() =>
+  import("../components/user/EmailValidation")
+);
+const ChangPassword = lazy(() => import("../components/user/ChangePassword"));
+const AdditionalJoin = lazy(() => import("../components/user/AdditionalJoin"));
+const CommunitySearchResult = lazy(() =>
+  import("../components/common/SearchResultTable")
+);
+const SCommunitySearchResult = lazy(() =>
+  import("../components/common/SCommunitySearchResult")
+);
 
 const Main = lazy(() => import("../pages/workspace/Main"));
 
@@ -96,12 +100,28 @@ const ThemeRoutes = [
       },
       { path: "/workspace", exact: true, element: <Main /> },
 
-            {path: "/login/oauth2/kakao", exact: true, element: <OAuth2RedirectHandler /> },
-            {path: "/emailValidation/:key", exact: true , element:<EmailValidation />},
-            {path: "/changePassword", exact : true, element : <ChangPassword />},
-            {path: "/additionalJoin", exact : true, element : <AdditionalJoin />},
-            {path: "/communitySearchResult", exact : true, element : <CommunitySearchResult />},
-            {path: "/surveyCommunitySearchResult", exact : true, element : <SCommunitySearchResult />}
+      {
+        path: "/login/oauth2/kakao",
+        exact: true,
+        element: <OAuth2RedirectHandler />,
+      },
+      {
+        path: "/emailValidation/:key",
+        exact: true,
+        element: <EmailValidation />,
+      },
+      { path: "/changePassword", exact: true, element: <ChangPassword /> },
+      { path: "/additionalJoin", exact: true, element: <AdditionalJoin /> },
+      {
+        path: "/communitySearchResult",
+        exact: true,
+        element: <CommunitySearchResult />,
+      },
+      {
+        path: "/surveyCommunitySearchResult",
+        exact: true,
+        element: <SCommunitySearchResult />,
+      },
     ],
   },
 ];
