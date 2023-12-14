844ca5cd6d7812c7f509a2589a5c4b03f4c1ead1
박소영 <48235429+soy6032@users.noreply.github.com>
2023-12-14 13:53:19 +0900

    [Feat] : admin login 추가
    [feat] : admin login 추가

---
+++ src/pages/admin/AdminLoginPage.jsx
@@ -0,0 +1,14 @@
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import * as React from "react";
+import AdminClaimList from "../../components/admin/AdminClaimList";
+import AdminHeader from "../../components/admin/AdminHeader";
+import style from "../../style/admin/AdminLogin.module.css"
+
+export default function AdminClaimListPage() {
+  return (
+    <div>
+      <div className={style.loginWrap}></div>
+    </div>
+  );
+}
