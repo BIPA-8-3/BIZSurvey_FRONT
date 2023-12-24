051e708f1f4ab8b6cae8b9ae2fb429b09364826c
WonsikHwang <j2s0007@gmail.com>
2023-12-25 00:09:48 +0900

    [Feat] : 공유 양식 초기화 기능 추가 & 설문 폼 CSS 수정
    공유 양식 초기화 기능 추가
    설문 폼 CSS 수정

--- src/pages/workspace/SharedSurvey.jsx
+++ src/pages/workspace/SharedSurvey.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import SurveyForm from "../../components/surveyCommunity/survey/SurveyForm";
 import { useLocation, useNavigate } from "react-router-dom";
 import call from "./api";
@@ -16,7 +16,7 @@ export default function SharedSurvey() {
   const [pass, setPass] = useState([]);
 
   // 설문지 데이터 [ 나의 설문 구조에 맞게 수정 ]
-  const [survey, setSurvey] = useState([]);
+  const [survey, setSurvey] = useState({});
 
   // 처음 렌더링 시 조회 및 pass true 처리
   useEffect(() => {
@@ -30,8 +30,7 @@ export default function SharedSurvey() {
           setPass(newPassArray);
         })
         .catch((error) => {
-          console.log(error);
-          console.log(error.response);
+          console.error(error);
         });
     }
   }, []);
@@ -47,6 +46,7 @@ export default function SharedSurvey() {
   ]);
   // 답변 설정 ( SurveyQuestion 사용함 )
   const handleSetAnswer = (questionId, userAnswer, answerType, url) => {
+    console.log([...answers]);
     // 현재 값의 복사 값으로 맵핑 진행
     setAnswers((pre) => {
       // 기존 Answers의 답변과 사용자가 체크한 답변을 비교
@@ -71,7 +71,7 @@ export default function SharedSurvey() {
     });
   };
 
-  // 응답 저장 [ 수정 필요 ] |
+  // 응답 저장
   const handleSubmitAnswer = async () => {
     // 필수 체크 검사
     const res = handleCheckAnswer();
@@ -102,8 +102,7 @@ export default function SharedSurvey() {
           state: { message: "설문 참여를 완료하였습니다." },
         });
       } catch (error) {
-        console.log("error", error);
-        console.log("error", error.response);
+        console.error(error);
         navigate("/authorization/result/external", {
           state: { message: error },
         });
@@ -111,11 +110,14 @@ export default function SharedSurvey() {
     }
   };
 
+  const handleResetClick = () => {
+    window.location.reload();
+  };
+
   // 저장 요청 시 실행 메소드, 필수 체크 Temp true로 초기화 후 검사 하는 메소드
   const handleCheckAnswer = () => {
     let result = true;
     let newPassArray = Array(survey.questions.length).fill(true);
-    console.log("survey", survey);
     survey.questions.map((question, index) => {
       if (question.isRequired) {
         const match = answers.find((ans) => ans.questionId === question.questionId);
@@ -149,15 +151,33 @@ export default function SharedSurvey() {
           type={"EXTERNAL"}
         />
         <div className={style.surveyBtnWrap}>
+          <Button onClick={handleResetClick}>양식 초기화</Button>
+
           <Button
             variant="contained"
             onClick={(e) => {
               handleSubmitAnswer();
             }}
+            sx={[
+              {
+                padding: "10px 25px",
+                backgroundColor: "#243579",
+                fontWeight: "bold",
+                marginBottom: "10px",
+                border: "1px solid #243579",
+                boxShadow: 0,
+                marginLeft: "5px",
+              },
+              {
+                ":hover": {
+                  border: "1px solid #1976d2",
+                  boxShadow: 0,
+                },
+              },
+            ]}
           >
             제출
           </Button>
-          <Button>양식 초기화</Button>
         </div>
       </div>
       <SharedFooter />
