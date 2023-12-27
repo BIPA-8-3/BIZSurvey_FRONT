ca7398a5cae0b6a08714b40cfbcf8632838c730d
WonsikHwang <j2s0007@gmail.com>
2023-12-27 11:28:05 +0900

    [Refactor] : SSE 제거
    SSE 제거

--- src/pages/workspace/WorkspaceContext.js
+++ src/pages/workspace/WorkspaceContext.js
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { checkPermissions } from './api';
-import SSEComponent from '../../components/workspace/SSEComponent';
+// import SSEComponent from '../../components/workspace/SSEComponent';
 
 const WorkspaceContext = createContext();
 
@@ -75,7 +75,7 @@ export const WorkspaceProvider = ({ children }) => {
             isPersonal,
         }}>
             <>
-                {<SSEComponent />}
+                {/* {<SSEComponent />} */}
                 {children}
             </>
         </WorkspaceContext.Provider >
